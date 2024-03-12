16256028b0f2db6d33ff38479ed07b5c335d12fe
Pauline Cvt <paulinecvt2021@gmail.com>
2024-03-12 15:43:25 +0100

    try to deploy

--- src/pages/CreatorProfilePage.jsx
+++ src/pages/CreatorProfilePage.jsx
@@ -1,10 +1,11 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import FavoritesList from "../components/favoritesList";
 
 import { Link } from "react-router-dom";
 import CreatorPosts from "../components/CreatorPosts";
-import FavoritesList from "../components/favoritesList";
+
 
 
 
