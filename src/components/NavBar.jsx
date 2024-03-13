8f2344dc5bf5d8291cdba966722deac6ed81c6dd
karkelo <dilanpapuc@gmail.com>
2024-03-13 11:01:05 +0100

    logoutbuttonnavbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faUser, faSignInAlt, faInfoCircle } from '@fortawesome/free-solid-svg-icons';
+import { faHome, faUser, faSignInAlt, faSignOutAlt, faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 import '../CSS/Navbar.css';
 import { AuthContext } from '../context/auth.context';
 
 function Navbar() {
+  const { isAuthenticated, logout } = useContext(AuthContext);
+
+  const handleLogout = () => {
+    logout(); // Assuming this function clears authentication state
+  };
+
   return (
     <nav className="navbar">
       <ul className="nav-links">
@@ -17,22 +23,34 @@ function Navbar() {
             </button>
           </Link>
         </li>
-        <li>
-          <Link to="/signup">
-            <button>
-              <FontAwesomeIcon icon={faUser} />
-              Sign Up
+        {!isAuthenticated && (
+          <li>
+            <Link to="/signup">
+              <button>
+                <FontAwesomeIcon icon={faUser} />
+                Sign Up
+              </button>
+            </Link>
+          </li>
+        )}
+        {!isAuthenticated && (
+          <li>
+            <Link to="/login">
+              <button>
+                <FontAwesomeIcon icon={faSignInAlt} />
+                Login
+              </button>
+            </Link>
+          </li>
+        )}
+        {isAuthenticated && (
+          <li>
+            <button onClick={handleLogout}>
+              <FontAwesomeIcon icon={faSignOutAlt} />
+              Logout
             </button>
-          </Link>
-        </li>
-        <li>
-          <Link to="/login">
-            <button>
-              <FontAwesomeIcon icon={faSignInAlt} />
-              Login
-            </button>
-          </Link>
-        </li>
+          </li>
+        )}
         <li>
           <Link to="/about">
             <button>
@@ -40,9 +58,6 @@ function Navbar() {
               About
             </button>
           </Link>
-        </li>
-        <li>
-          
         </li>
       </ul>
     </nav>
